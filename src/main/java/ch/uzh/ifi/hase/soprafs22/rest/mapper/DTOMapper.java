e013dd4a6d1ae722301d41c68e71dbd893c128ec
mattialeonetti <91195886+mattialeonetti@users.noreply.github.com>
2022-03-17 11:27:21 +0100

    test commit for heroku

--- src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/rest/mapper/DTOMapper.java
@@ -6,17 +6,6 @@ import ch.uzh.ifi.hase.soprafs22.rest.dto.UserPostDTO;
 import org.mapstruct.*;
 import org.mapstruct.factory.Mappers;
 
-/**
- * DTOMapper
- * This class is responsible for generating classes that will automatically
- * transform/map the internal representation
- * of an entity (e.g., the User) to the external/API representation (e.g.,
- * UserGetDTO for getting, UserPostDTO for creating)
- * and vice versa.
- * Additional mappers can be defined for new entities.
- * Always created one mapper for getting information (GET) and one mapper for
- * creating information (POST).
- */
 @Mapper
 public interface DTOMapper {
 
