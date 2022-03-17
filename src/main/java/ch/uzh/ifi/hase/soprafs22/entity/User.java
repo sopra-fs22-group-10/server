117346164b3f0102a65dbd59717c56af1f8927cb
mattialeonetti <mattia.leonetti@uzh.ch>
2022-03-17 11:17:38 +0100

    test push for heroku

--- src/main/java/ch/uzh/ifi/hase/soprafs22/entity/User.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs22/entity/User.java
@@ -5,7 +5,16 @@ import ch.uzh.ifi.hase.soprafs22.constant.UserStatus;
 import javax.persistence.*;
 import java.io.Serializable;
 
-
+/**
+ * Internal User Representation
+ * This class composes the internal representation of the user and defines how
+ * the user is stored in the database.
+ * Every variable will be mapped into a database field with the @Column
+ * annotation
+ * - nullable = false -> this cannot be left empty
+ * - unique = true -> this value must be unqiue across the database -> composes
+ * the primary key
+ */
 @Entity
 @Table(name = "USER")
 public class User implements Serializable {
