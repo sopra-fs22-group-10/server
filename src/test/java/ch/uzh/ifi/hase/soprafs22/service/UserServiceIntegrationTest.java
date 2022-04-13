022fe2b9553b0940cc31f6a9bb7a7b4d3b56524d
timvorburger <tim.vorburger@uzh.ch>
2022-04-13 11:06:15 +0200

    bugfix UserServiceIntegrationTest

--- src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs22/service/UserServiceIntegrationTest.java
@@ -22,6 +22,7 @@ import java.time.LocalDate;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 
+
 /**
  * Test class for the UserResource REST resource.
  *
